94812e8bfb0265c0dbfedc75f8b0678c453a1f78
MikhailShokun <minishokun@gmail.com>
2022-01-07 15:25:16 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// -------------tabs-------------
+
 let tab = function() {
     let tabNav = document.querySelectorAll('.tabs-nav__item'),
         tabContent = document.querySelectorAll('.tab'),
@@ -25,3 +27,78 @@ let tab = function() {
 };
 
 tab();
+
+// --------------accordeon------------------
+
+let accord = function() {
+
+    function initAccordeon() {
+        let firstSectionBodyHeight = document.querySelector('.accordeon__item .accordeon__body > *').clientHeight
+
+        document.querySelector('.accordeon__item .accordeon__body').style.maxHeight = firstSectionBodyHeight + 'px'
+    }
+
+    initAccordeon()
+
+    let accordeonHeaderClickHandler = function(e) {
+        document.querySelectorAll('.accordeon__item').forEach(function(section) {
+            section.querySelector('.accordeon__body').style.maxHeight = '0px'
+        })
+
+        let accordeonItem = e.target.closest('.accordeon__item')
+        let insideElHeight = accordeonItem.querySelector('.accordeon__body > *').clientHeight
+
+        accordeonItem.querySelector('.accordeon__body').style.maxHeight = insideElHeight + 'px'
+    }
+    document.querySelectorAll('.accordeon__item').forEach(function(section) {
+        section.addEventListener('click', accordeonHeaderClickHandler)
+    })
+};
+
+accord();
+
+// --------------slider-V1--------------
+
+
+let images = document.querySelectorAll('.infinity-slider img');
+let current = 0;
+
+
+function slider() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.add('opacity0');
+    }
+    images[current].classList.remove('opacity0');
+}
+
+
+
+document.querySelector('.slider__btn-right').onclick = function() {
+    if (current + 1 == images.length) {
+        current = 0;
+    } else {
+        current++;
+    }
+    slider();
+};
+
+
+document.querySelector('.slider__btn-left').onclick = function() {
+    if (current - 1 == -1) {
+        current = images.length - 1;
+    } else {
+        current--;
+    }
+    slider();
+};
+
+
+
+document.querySelector('.infinity-slider').onclick = function() {
+    if (current + 1 == images.length) {
+        current = 0;
+    } else {
+        current++;
+    }
+    slider();
+};
